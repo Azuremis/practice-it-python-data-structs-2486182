49b6b088983a35f9a4987cfb7de2fcee2aa3770c
Azuremis <azuremismachine@gmail.com>
2023-02-26 14:22:59 +0000

    Add tracking product orders

--- 04_02b.py
+++ 04_02b.py
@@ -1,7 +1,22 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+from pprint import pprint
+from csv import reader
 
 def main():
     #add code here
+    #read data/OrderItems.csv
+    #Create workable objects with each line
+    # OrderItemID,OrderID,ProductID,Quantity
+    res = defaultdict(int)
+    with open('data/OrderItems.csv', 'r') as open_csv:
+        read = reader(open_csv)
+        OrderItem = namedtuple("OrderItem", next(read))
+        for line in read:
+            Item = OrderItem(*line)
+            res[Item.ProductID] += int(Item.Quantity)
+    pprint(dict(res))
+
+
 
     return
 
