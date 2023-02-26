35d6a1601b4eabef96835099f14109e778c2cf9d
Azuremis <azuremismachine@gmail.com>
2023-02-26 13:17:23 +0000

    Add toDoList implementation

--- 02_03b.py
+++ 02_03b.py
@@ -1,6 +1,42 @@
+from collections import deque
+
+class Task(object):
+    def __init__(self, taskDesc, hasPriority=False):
+        self.hasPriority = hasPriority
+        self.taskDesc = taskDesc
+    def __str__(self):
+        return f"Task: {self.taskDesc}, Priority: {self.hasPriority} "
+
+toDoList = deque()
+
+def add_task(task):
+    if task.hasPriority:
+        toDoList.appendleft(task)
+    else:
+        toDoList.append(task)
+
+def do_task():
+    return toDoList.popleft()
+
+def print_tasks():
+    for t in toDoList:
+        print(t.taskDesc)
+    print()
+
+
 
 def main():
-    #add code here
+    print(toDoList)
+    add_task(Task("Flow through foundational practices"))
+    add_task(Task("Take a break", True))
+    add_task(Task("Finish breakfast"))
+    print_tasks()
+    add_task(Task("Prioritise quests", True))
+    print_tasks()
+    do_task()
+    add_task(Task("Tidy strategy board", True))
+    print_tasks()
+
     return
 
 if __name__ == "__main__":
