bc4dfc95c88e1e85284f49f16f088a441f7125f4
Azuremis <azuremismachine@gmail.com>
2023-02-26 12:28:44 +0000

    Add palindrome check

--- 02_02b.py
+++ 02_02b.py
@@ -1,6 +1,19 @@
+from collections import deque
+
+
+def palindrome_checker(word):
+    # create a deque
+    word_deque = deque(word)
+    # check if the deque is a palindrome
+    while len(word_deque) > 1:
+        print(word_deque)
+        if word_deque.popleft() != word_deque.pop():
+            return False
+    return True
 
 def main():
-    #add code here
+    
+    print(palindrome_checker("racecar"))
     return
 
 if __name__ == "__main__":
