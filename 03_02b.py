9cc1d6bd395dbf09398bebb3255095016b3ae5d3
Azuremis <azuremismachine@gmail.com>
2023-02-26 13:45:55 +0000

    Read customer data

--- 03_02b.py
+++ 03_02b.py
@@ -1,8 +1,18 @@
+from collections import namedtuple
+from csv import reader
 
 def main():
-    #add code here
     #read data/Customer.csv
     #Create workable objects with each line
+    # CustomerID,FirstName,LastName,Email,Phone,Address,City,State,Zipcode
+    with open('data/Customer.csv', 'r') as open_csv:
+        read = reader(open_csv)
+        Person = namedtuple("Person", next(read))
+        for line in read:
+            person = Person(*line)
+            print(person.CustomerID, person.FirstName, person.LastName)
+    
+
     return
 
 if __name__ == "__main__":
