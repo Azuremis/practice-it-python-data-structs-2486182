f3e6054b8c8474bf47786b580636c9675ba5a9b3
Azuremis <azuremismachine@gmail.com>
2023-02-26 12:21:55 +0000

    Add order tracking

--- 02_01b.py
+++ 02_01b.py
@@ -1,7 +1,19 @@
-
+from collections import deque
 def main():
-    #add code here
-    return
+    
+    # create a deque
+    foods = deque(maxlen=5)
+    # add items to the deque
+    foods.append("STA001") 
+    ordered_foods = ["DES003", "STA002", "ENT004", "ENT001"]
+    foods.extend(ordered_foods)
+    # add an item to the left of the deque
+    foods.appendleft("DES005")
+    # add an item to the right of the deque
+    foods.append("DES002")
+    
+    # print the deque
+    print(foods)
 
 if __name__ == "__main__":
     main()
